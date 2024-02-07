c80ab87f6248fb3c4c4dcdabb44ddb58b0c7031c
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-02-07 18:59:21 -0300

    Clase 2 - Crear proyecto vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Mi segunda clase de FEIII</h1>
+      <li>Alumno 1</li>
+      <li>Alumno 2</li>
+      <li>Alumno 3</li>
+      <li>Alumno 4</li>
+      <li>Alumno 5</li>
+    </>
+  )
+}
+
+export default App
